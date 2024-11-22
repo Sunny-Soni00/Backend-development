a1633f1d1a7b3c9081a58dbf1b9713eab40022b4
Sunny <sunny.s@atriauniversity.edu.in>
2024-11-22 13:54:23 +0530

    done

--- Task2/task2.py
+++ Task2/task2.py
@@ -1,43 +1,43 @@
-# # part 1: Basic Authentication
-# from flask import Flask, request
+# part 1: Basic Authentication
+from flask import Flask, request
 
-# app = Flask(__name__)
+app = Flask(__name__)
 
-# # Homepage
-# @app.route('/home', methods=['GET'])
-# def home():
-#     return "Welcome to the homepage"
+# Homepage
+@app.route('/home', methods=['GET'])
+def home():
+    return "Welcome to the homepage"
 
-# # Define valid credentials
-# VALID_USERNAME = "user"
-# VALID_PASSWORD = "password"
+# Define valid credentials
+VALID_USERNAME = "user"
+VALID_PASSWORD = "password"
 
-# @app.route('/basic-auth', methods=['GET'])
-# def basic_auth():
-#     auth = request.authorization
-#     if auth and auth.username == VALID_USERNAME and auth.password == VALID_PASSWORD:
-#         message = "Welcome"
-#         return (message,200)
-#     else:
-#         message = "Unauthorized access"
-#         return (message, 401)
+@app.route('/basic-auth', methods=['GET'])
+def basic_auth():
+    auth = request.authorization
+    if auth and auth.username == VALID_USERNAME and auth.password == VALID_PASSWORD:
+        message = "Welcome"
+        return (message,200)
+    else:
+        message = "Unauthorized access"
+        return (message, 401)
 
-# # part 2: Bearer Token Authentication
-# VALID_TOKEN = "Token"
+# part 2: Bearer Token Authentication
+VALID_TOKEN = "Token"
 
-# @app.route('/bearer-auth', methods=['GET'])
-# def bearer_auth():
-#     auth_header = request.headers.get('Authorization')
-#     if auth_header and auth_header.startswith('Bearer '):
-#         token = auth_header.split(' ')[1]
-#         if token == VALID_TOKEN:
-#             message = "Access granted"
-#             return message, 200
-#     message = "Unauthorized access"
-#     return message, 401
+@app.route('/bearer-auth', methods=['GET'])
+def bearer_auth():
+    auth_header = request.headers.get('Authorization')
+    if auth_header and auth_header.startswith('Bearer '):
+        token = auth_header.split(' ')[1]
+        if token == VALID_TOKEN:
+            message = "Access granted"
+            return message, 200
+    message = "Unauthorized access"
+    return message, 401
 
-# if __name__ == '__main__':
-#     app.run(debug=True)
+if __name__ == '__main__':
+    app.run(debug=True)
 
 # part 3: OAuth 2.0 Authentication
 
